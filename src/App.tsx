b57fbd809275f6c782f191d0502404e954da0d02
Andreas Strid <andreas.strid.cib@gmail.com>
2025-02-17 13:27:17 +0100

    Removed double folder

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import { lazy, Suspense } from "react";
+
+const CatInBoxPage = lazy(() => import("./CatInBox/CatInBoxPage"));
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/catinbox"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <CatInBoxPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
